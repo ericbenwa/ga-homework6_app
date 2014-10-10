dd6215910365a3f59cad863ff91d0438e0ab3931
Eric Benoit <benwa02@gmail.com>
2014-10-09 22:03:36 -0400

    Checking user input with user database to sign in.

--- user.rb
+++ user.rb
@@ -12,18 +12,27 @@ class User
 
 	def self.sign_in
 		puts "Enter your username."
-		entered_user = gets.chomp
+		$entered_user = gets.chomp
 
 		puts "Enter your password."
-		entered_password = gets.chomp
+		$entered_password = gets.chomp
 
-		if @user == entered_user && @password == entered_password
-			puts "Success! You are now signed in."
-			# signed_in = true
-			puts $session = [user, password]
-		else
-			puts "Incorrect! Your username of password is incorrect." # Enhance by asking the user to try again.
+		$user_list.each do |user, password|
+			if $entered_user == user && $entered_password == password
+				puts "Signed in as #{user}."
+				break
+			else
+				puts "Incorrect! Your username of password is incorrect."
+			end
 		end
+
+		# if @user == entered_user && @password == entered_password
+		# 	puts "Success! You are now signed in."
+		# 	# signed_in = true
+		# 	puts $session = [user, password]
+		# else
+		# 	puts "Incorrect! Your username of password is incorrect." # Enhance by asking the user to try again.
+		# end
 	end
 
 	def sign_out
@@ -35,18 +44,18 @@ class User
 		$user_list.each { |user, password| puts "#{user} and #{password}"}
 	end
 
-	def self.testloop
-		$entered_userr = "eric"
-		$entered_passwordd = "pw"
+	# def self.testloop
+	# 	$entered_userr = "eric"
+	# 	$entered_passwordd = "pw"
 
-		$user_list.each do |user, password|
-			if $entered_userr == user && $entered_passwordd == password
-				puts "signed in"
-			else
-				puts "incorrect"
-			end
-		end
-	end
+	# 	$user_list.each do |user, password|
+	# 		if $entered_userr == user && $entered_passwordd == password
+	# 			puts "signed in"
+	# 		else
+	# 			puts "incorrect"
+	# 		end
+	# 	end
+	# end
 
 	def tweet
 		puts "we tweeting."
